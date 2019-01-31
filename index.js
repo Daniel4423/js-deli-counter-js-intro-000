e63cf48fc5dca2d8de19326037064ca0fa8158d4
Daniel Bailey <daniel4423@gmail.com>
2019-01-31 21:51:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-function takeANumber(currentLine, newName) {
-  currentLine.push(newName)
-  var position = currentLine.length
+function takeANumber(katzDeliLine, newName) {
+  katzDeliLine.push(newName)
+  var position = katzDeliLine.length
   return `Welcome, ${newName}. You are number ${position} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length = 0) {
+    return `There is nobody waiting to be served.`;
+  } else {
+    var nextCustomer = katzDeliLine[0];
+    katzDeliLine.shift();
+    return nextCustomer;
+  }
 }
