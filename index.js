855342007b3d82bdbe78065a97f0f6c15e93e241
Daniel Bailey <daniel4423@gmail.com>
2019-01-31 21:44:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function takeANumber(currentLine, newName) {
+  currentLine.append(newName)
   var position = currentLine.length + 1
   return `Welcome, ${newName}. You are number ${position} in line.`
 }
